9d7bfa2e0844d1fecde8e31b8476c6fe74cbfa6d
thien.anh.nguyen <anhthien@tgm.vn>
2022-06-20 13:45:07 +0700

    update demo - refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,38 +3,47 @@ import type { AppProps } from 'next/app'
 import {useCallback, useEffect, useState} from "react";
 import LoginPage from './login'
 
+const isClient = () =>
+    typeof window !== "undefined" && typeof document !== "undefined";
+
+const Pending = "pending";
+const LoggedIn = "loggedin";
+const LoggedOut = "loggedout";
+
+type UserState = typeof Pending | typeof LoggedIn | typeof LoggedOut;
+
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null)
+  const [loginState, setLoginState] = useState<UserState>(Pending);
 
   const checkLogin = useCallback(() => {
-    setTimeout(() => setIsLoggedIn(true), 1000)
-  }, [])
+    setTimeout(() => setLoginState(LoggedIn), 1000);
+  }, []);
 
   useEffect(() => {
-    if (typeof window === 'undefined' || isLoggedIn === true) {
-      return
-    }
+    if (loginState === LoggedIn) return;
 
-    checkLogin()
-  }, [checkLogin, isLoggedIn])
+    checkLogin();
+  }, [checkLogin, loginState]);
 
   // @ts-ignore
-  const getLayout = Component.getLayout ?? ((page) => page)
+  const getLayout = Component.getLayout ?? ((page) => page);
 
-  const renderPage = () => {
-    if (isLoggedIn === null) {
-      if (typeof window !== 'undefined') {
-        // CSR
-        console.log('CSR')
-        return null
-      }
-      // SSR
-      console.log('SSR')
-      return <LoginPage />
+  if (isClient()) {
+    if (loginState === Pending) {
+      return getLayout(<Component {...pageProps} />)
     }
-    return isLoggedIn ? (<>{getLayout(<Component {...pageProps} />)}</>) : <LoginPage />
+
+    return loginState === LoggedIn ? (
+        getLayout(<Component {...pageProps} />)
+    ) : (
+        <LoginPage />
+    );
   }
-  return renderPage()
+  // Un-comment this to see the error, this case is for case google crawl bot on server-side only, Or do you have a better suggestion for this SEO?
+  // if (loginState === Pending) {
+  //   return <LoginPage/>
+  // }
+  return getLayout(<Component {...pageProps} />);
 }
 
 export default MyApp
